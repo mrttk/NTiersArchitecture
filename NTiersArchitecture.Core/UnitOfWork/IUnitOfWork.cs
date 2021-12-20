f6f9d24de68938e090c4ce836b820bd2567a7d4b
= <=>
2021-12-21 01:50:38 +0200

    Add UnitOfWork contracts

---
+++ NTiersArchitecture.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using NTiersArchitecture.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTiersArchitecture.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IProductService Products { get; }
+        ICategoryService Categories { get; }
+
+        Task CommitAsync();
+        void Commit();
+    }
+}
