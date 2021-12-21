98b426a251d93ae78ed01e9c1134672ae5fca538
= <=>
2021-12-21 14:39:45 +0200

    Add DbContext for Application

---
+++ NTiersArchitecture.Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTiersArchitecture.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NTiersArchitecture.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
