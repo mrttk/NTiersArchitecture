9dac31049626c9057a41a94050e3cede84739395
= <=>
2021-12-23 02:44:45 +0200

    Update ProductService.cs for dry principle

--- NTiersArchitecture.Service/Services/ProductService.cs
+++ NTiersArchitecture.Service/Services/ProductService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NTiersArchitecture.Core.Entity;
+using NTiersArchitecture.Core.Repositories;
 using NTiersArchitecture.Core.Services;
 using NTiersArchitecture.Core.UnitOfWork;
 using NTiersArchitecture.Data;
@@ -11,72 +12,15 @@ using System.Threading.Tasks;
 
 namespace NTiersArchitecture.Service.Services
 {
-    public class ProductService : IProductService
+    public class ProductService : Service<Product>, IProductService
     {
-        private readonly AppDbContext _context;
-        private readonly IUnitOfWork _unitOfWork;
-        public ProductService(IUnitOfWork unitOfWork)
+        public ProductService(IUnitOfWork unitOfWork, IRepository<Product> repository) : base(unitOfWork, repository)
         {
-            _unitOfWork = unitOfWork;
-        }
-        public async Task<Product> AddAsync(Product entity)
-        {
-            await _unitOfWork.Products.AddAsync(entity);
-            await _unitOfWork.CommitAsync();
-            return entity;
-        }
-
-        public async Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
-        {
-            await _unitOfWork.Products.AddRangeAsync(entities);
-            await _unitOfWork.CommitAsync();
-
-            return entities;
-        }
-
-        public async Task<IEnumerable<Product>> Where(Expression<Func<Product, bool>> predicate)
-        {
-            return await _unitOfWork.Products.Where(predicate);
-        }
-
-        public async Task<IEnumerable<Product>> GetAllAsync()
-        {
-            return await _unitOfWork.Products.GetAllAsync();
-        }
-
-        public async Task<Product> GetByIdAsync(int id)
-        {
-            return await _unitOfWork.Products.GetByIdAsync(id);
         }
 
         public async Task<Product> GetWithCategoryByIdAsync(int productId)
         {
             return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
-
-        }
-
-        public void Remove(Product entity)
-        {
-            _unitOfWork.Products.Remove(entity);
-            _unitOfWork.Commit();
-        }
-
-        public void RemoveRange(IEnumerable<Product> entities)
-        {
-            _unitOfWork.Products.RemoveRange(entities);
-            _unitOfWork.Commit();
-        }
-
-        public async Task<Product> SingleOrDefault(Expression<Func<Product, bool>> predicate)
-        {
-            return await _unitOfWork.Products.SingleOrDefault(predicate);
-        }
-
-        public Product Update(Product entity)
-        {
-            var updateProduct = _unitOfWork.Products.Update(entity);
-            _unitOfWork.Commit();
-            return updateProduct;
         }
     }
 }
