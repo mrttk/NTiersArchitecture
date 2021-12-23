2b657fdfd9f30f4bb76a59b236b89fcff3455eae
= <=>
2021-12-23 17:43:17 +0200

    Add GetAll, GetById methods with using CategoryDto in CategoryController

--- NTiersArchitecture.API/Controllers/CategoryController.cs
+++ NTiersArchitecture.API/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NTiersArchitecture.API.DTOs;
 using NTiersArchitecture.Core.Services;
 using System;
 using System.Collections.Generic;
@@ -13,16 +15,25 @@ namespace APINew.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
-        public CategoryController(ICategoryService categoryService)
+        private readonly IMapper _mapper;
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var categories = await _categoryService.GetAllAsync();
-            return Ok(categories);
+            return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return Ok(_mapper.Map<CategoryDto>(category));
         }
     }
 }
