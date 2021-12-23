b722f372fdbe194517bea3f8c392bbba34480a4c
= <=>
2021-12-23 22:03:19 +0200

    Add CategoryWithProductsDto

---
+++ NTiersArchitecture.API/DTOs/CategoryWithProductsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTiersArchitecture.API.DTOs
+{
+    public class CategoryWithProductsDto : CategoryDto
+    {
+        public ICollection<ProductDto> Products { get; set; }
+    }
+}
