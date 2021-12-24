9e2364318637450e4c3362376057469f4d1412d3
= <=>
2021-12-24 02:54:20 +0200

    Add new map transformation for ProductWithCategoryDto in MapProfile.cs

--- NTiersArchitecture.API/Mapping/MapProfile.cs
+++ NTiersArchitecture.API/Mapping/MapProfile.cs
@@ -20,6 +20,9 @@ namespace NTiersArchitecture.API.Mapping
 
             CreateMap<Product, ProductDto>();
             CreateMap<ProductDto, Product>();
+
+            CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<ProductWithCategoryDto, Product>();
         }
     }
 }
