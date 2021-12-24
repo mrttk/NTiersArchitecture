93161d7516f40dd49f48797b08485c66a8a40ce5
= <=>
2021-12-24 22:26:50 +0200

    Add Person entity and relatinal process

--- NTiersArchitecture.Data/Migrations/20211224202117_AddPerson.Designer.cs
+++ NTiersArchitecture.Data/Migrations/20211224202117_AddPerson.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NTiersArchitecture.Data;
 
 namespace NTiersArchitecture.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211224202117_AddPerson")]
+    partial class AddPerson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
