f17c1d31ad10d6dce0ef50421c14bbdbdd1a955b
= <=>
2021-12-25 06:41:29 +0200

    Add personDto and mapping configuration

--- NTiersArchitecture.API/Mapping/MapProfile.cs
+++ NTiersArchitecture.API/Mapping/MapProfile.cs
@@ -23,6 +23,9 @@ namespace NTiersArchitecture.API.Mapping
 
             CreateMap<Product, ProductWithCategoryDto>();
             CreateMap<ProductWithCategoryDto, Product>();
+
+            CreateMap<Person, PersonDto>();
+            CreateMap<PersonDto, Person>();
         }
     }
 }
