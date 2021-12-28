723099d805cb329ff236ea7c0b2a33d746c30c85
= <=>
2021-12-28 18:05:23 +0200

    Add data transfer objects

---
+++ NTiersArchitecture.Web/DTOs/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTiersArchitecture.Web.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1,double.MaxValue)]
+        public decimal Price { get; set; }
+        [Range(1,int.MaxValue)]
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
