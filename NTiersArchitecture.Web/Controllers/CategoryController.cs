5bb5d95f2ac3c1f8a3d2b459403351b330b86609
= <=>
2021-12-28 18:49:51 +0200

    Add CategoryController

---
+++ NTiersArchitecture.Web/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NTiersArchitecture.Web.DTOs;
+using NTiersArchitecture.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTiersArchitecture.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+    }
+}
